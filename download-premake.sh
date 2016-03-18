2569a2ea78e02cd8eba9b65e45d31b1d4cb826b2
BloodyProg <bloody.prog@gmail.com>
2016-03-17 22:49:38 -0400

    test

--- download-premake.sh
+++ download-premake.sh
@@ -10,6 +10,14 @@ if [[ $os_name == "osx" ]]; then
     wget $url/premake-5.0.0-alpha8-macosx.tar.gz
     tar -xf premake-5.0.0-alpha8-macosx.tar.gz
 else
-    wget $url/premake-5.0.0-alpha8-linux.tar.gz
-    tar -xf premake-5.0.0-alpha8-linux.tar.gz
+    github=https://github.com/premake/premake-core.git
+    branch=release
+
+    git clone --depth=1 --recursive -b $branch --single-branch $github premake-core
+
+    cd premake-core
+    make -f Bootstrap.mak linux
+    cd ..
+
+    cp premake-core/bin/release/premake5 premake5
 fi
