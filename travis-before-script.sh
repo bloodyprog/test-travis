05578dc8cbc8993f05d9eee84cb440aa746c1e2b
BloodyProg <bloody.prog@gmail.com>
2016-06-19 12:33:09 -0400

    tweak before script

--- travis-before-script.sh
+++ travis-before-script.sh
@@ -4,21 +4,21 @@ if [[ "$LINUX_TEST" == "main" ]]; then
     gcc-5 --version
 fi
 
-if [[ "$LINUX_TEST" == "clang-3.6" ]]; then
+if [[ "$LLVM_VERSION" == "3.6.2" ]]; then
     ls /usr/lib/llvm-3.6
     ls /usr/lib/llvm-3.6/include
     ls /usr/lib/llvm-3.6/lib/*clang*
     clang-3.6 --version
 fi
 
-if [[ "$LINUX_TEST" == "clang-3.7" ]]; then
+if [[ "$LLVM_VERSION" == "3.7.1" ]]; then
     ls /usr/lib/llvm-3.7
     ls /usr/lib/llvm-3.7/include
     ls /usr/lib/llvm-3.7/lib/*clang*
     clang-3.7 --version
 fi
 
-if [[ "$LINUX_TEST" == "clang-3.8" ]]; then
+if [[ "$LLVM_VERSION" == "3.8.0" ]]; then
     ls /usr/lib/llvm-3.8
     ls /usr/lib/llvm-3.8/include
     ls /usr/lib/llvm-3.8/lib/*clang*
