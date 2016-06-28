1c819f3af980396a3cd30be3fcd5dc46958915b7
BloodyProg <bloody.prog@gmail.com>
2016-06-27 22:30:01 -0400

    tweak

--- travis-before-install.sh
+++ travis-before-install.sh
@@ -10,18 +10,21 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     brew install sqlite
 fi
 
+llvm_root=llvm-$LLVM_VERSION
+llvm_archive=llvm-$LLVM_VERSION.tar.xz
+
 if [[ "${TRAVIS_OS_NAME}" = "linux" ]]; then
-    if [[ ! -d "llvm-$LLVM_VERSION" ]]; then
-      echo "$(tput setaf 6)$(tput bold)Downloading...(tput sgr 0)"
-      wget -O llvm-$LLVM_VERSION.tar.xz http://llvm.org/releases/$LLVM_VERSION/clang+llvm-$LLVM_VERSION-x86_64-linux-gnu-ubuntu-14.04.tar.xz
-      mkdir llvm-$LLVM_VERSION
-      xzcat llvm-$LLVM_VERSION.tar.xz | tar -xvf - --strip 1 -C llvm-$LLVM_VERSION
+    if [[ ! -d $llvm_root ]]; then
+      echo "$(tput setaf 6)$(tput bold)Downloading...$(tput sgr 0)"
+      wget -O $llvm_archive http://llvm.org/releases/$LLVM_VERSION/clang+llvm-$LLVM_VERSION-x86_64-linux-gnu-ubuntu-14.04.tar.xz
+      mkdir $llvm_root
+      xzcat $llvm_archive | tar -xvf - --strip 1 -C $llvm_root
     else
-      echo "$(tput setaf 6)$(tput bold)Already Downloaded(tput sgr 0)"
+      echo "$(tput setaf 6)$(tput bold)Already Downloaded$(tput sgr 0)"
     fi
 
-    llvm-$LLVM_VERSION/bin/llvm-config --version
-    export LLVM_CONFIG="llvm-$LLVM_VERSION/bin/llvm-config"
+    $llvm_root/bin/llvm-config --version
+    export LLVM_CONFIG="$llvm_root/bin/llvm-config"
 fi
 
 ./download-premake.sh $TRAVIS_OS_NAME
