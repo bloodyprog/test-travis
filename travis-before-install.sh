7e3451dc5beaffd13cafd932511e35c4fe062398
BloodyProg <bloody.prog@gmail.com>
2016-06-27 22:41:51 -0400

    tweak colours

--- travis-before-install.sh
+++ travis-before-install.sh
@@ -15,12 +15,12 @@ llvm_archive=llvm-$LLVM_VERSION.tar.xz
 
 if [[ "${TRAVIS_OS_NAME}" = "linux" ]]; then
     if [[ ! -d $llvm_root ]]; then
-      echo "$(tput setaf 6)$(tput bold)Downloading...$(tput sgr 0)"
+      echo "$(tput setaf 3)$(tput bold)Downloading...$(tput sgr 0)"
       wget -O $llvm_archive http://llvm.org/releases/$LLVM_VERSION/clang+llvm-$LLVM_VERSION-x86_64-linux-gnu-ubuntu-14.04.tar.xz
       mkdir $llvm_root
       xzcat $llvm_archive | tar -xvf - --strip 1 -C $llvm_root
     else
-      echo "$(tput setaf 6)$(tput bold)Already Downloaded$(tput sgr 0)"
+      echo "$(tput setaf 2)$(tput bold)Already Downloaded$(tput sgr 0)"
     fi
 
     $llvm_root/bin/llvm-config --version
