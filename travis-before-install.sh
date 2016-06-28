a2dd22a4fb824a1dca7cf7e2f56ca388ce9f52ec
BloodyProg <bloody.prog@gmail.com>
2016-06-27 23:45:37 -0400

    moving dependencies in a sub folder

--- travis-before-install.sh
+++ travis-before-install.sh
@@ -2,5 +2,17 @@
 
 echo "$(tput setaf 6)$(tput bold)Travis: Before Install$(tput sgr 0)"
 
+dependencies_root="dependencies"
+
+if [[ ! -d $dependencies_root ]]; then
+    mkdir $dependencies_root
+fi
+
+pushd $dependencies_root > /dev/null
+
+ls -l
+
 ./install-llvm.sh $TRAVIS_OS_NAME
 ./install-premake.sh $TRAVIS_OS_NAME
+
+popd > /dev/null
