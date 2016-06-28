094b36aeb62fe7bd71be26c446560c1e18839f1f
BloodyProg <bloody.prog@gmail.com>
2016-06-27 22:46:52 -0400

    ask

--- travis-before-install.sh
+++ travis-before-install.sh
@@ -19,6 +19,7 @@ if [[ "${TRAVIS_OS_NAME}" = "linux" ]]; then
       wget -O $llvm_archive http://llvm.org/releases/$LLVM_VERSION/clang+llvm-$LLVM_VERSION-x86_64-linux-gnu-ubuntu-14.04.tar.xz
       mkdir $llvm_root
       xzcat $llvm_archive | tar -xvf - --strip 1 -C $llvm_root
+      echo "$(tput setaf 3)$(tput bold)Download Complete$(tput sgr 0)"
     else
       echo "$(tput setaf 2)$(tput bold)Already Downloaded$(tput sgr 0)"
     fi
