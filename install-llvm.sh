2fe811f50e4d6a8081fdf99a0223860fbe23cf37
BloodyProg <bloody.prog@gmail.com>
2016-06-27 23:24:05 -0400

    tweaks

--- install-llvm.sh
+++ install-llvm.sh
@@ -4,7 +4,7 @@ set -e
 
 os_name=$1
 
-echo "$(tput setaf 6)$(tput bold)Dependnecy: LLVM$(tput sgr 0)"
+echo "$(tput setaf 6)$(tput bold)> [llvm]$(tput sgr 0)"
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
 
@@ -14,19 +14,18 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     brew install llvm38
     brew install sqlite
 
-elif [[ "${TRAVIS_OS_NAME}" = "linux" ]]; then
+else
 
     llvm_root=llvm-$LLVM_VERSION
     llvm_archive=llvm-$LLVM_VERSION.tar.xz
 
     if [[ ! -d $llvm_root ]]; then
-      echo "$(tput setaf 3)$(tput bold)> downloading binaries$(tput sgr 0)"
+      echo "$(tput setaf 3)$(tput bold)> [llvm] downloading binaries$(tput sgr 0)"
       wget -O $llvm_archive http://llvm.org/releases/$LLVM_VERSION/clang+llvm-$LLVM_VERSION-x86_64-linux-gnu-ubuntu-14.04.tar.xz
       mkdir $llvm_root
       xzcat $llvm_archive | tar -xvf - --strip 1 -C $llvm_root
-      echo "$(tput setaf 3)$(tput bold)> download complete$(tput sgr 0)"
     else
-      echo "$(tput setaf 2)$(tput bold)> already downloaded$(tput sgr 0)"
+      echo "$(tput setaf 2)$(tput bold)> [llvm] already downloaded$(tput sgr 0)"
     fi
 
     $llvm_root/bin/llvm-config --version
--- install-premake.sh
+++ install-premake.sh
@@ -4,26 +4,40 @@ set -e
 
 os_name=$1
 
-echo "$(tput setaf 6)$(tput bold)Dependency: Premake$(tput sgr 0)"
-
-url=https://github.com/premake/premake-core/releases/download/v5.0.0-alpha8
+echo "$(tput setaf 6)$(tput bold)> [premake]$(tput sgr 0)"
 
 if [[ $os_name == "osx" ]]; then
+
+    url=https://github.com/premake/premake-core/releases/download/v5.0.0-alpha8
+
     wget $url/premake-5.0.0-alpha8-macosx.tar.gz
     tar -xf premake-5.0.0-alpha8-macosx.tar.gz
+
 else
-    echo "$(tput setaf 3)$(tput bold)> cloning from github$(tput sgr 0)"
 
-    github=https://github.com/premake/premake-core.git
-    branch=release
+    premake_root=premake-core
 
-    git clone --depth=1 --recursive -b $branch --single-branch $github premake-core
+    if [[ ! -d $premake_root ]]; then
+        echo "$(tput setaf 3)$(tput bold)> [premake] cloning from github$(tput sgr 0)"
 
-    echo "$(tput setaf 3)$(tput bold)> compiling$(tput sgr 0)"
+        github=https://github.com/premake/premake-core.git
+        branch=release
 
-    cd premake-core
-    make -f Bootstrap.mak linux
-    cd ..
+        git clone --depth=1 --recursive -b $branch --single-branch $github premake-core
+    else
+        echo "$(tput setaf 2)$(tput bold)> [premake] already cloned from github$(tput sgr 0)"
+    fi
 
-    cp premake-core/bin/release/premake5 premake5
+    if [[ ! -e $premake_root/bin/release/premake5 ]]; then
+        echo "$(tput setaf 3)$(tput bold)> [premake] compiling$(tput sgr 0)"
+
+        cd $premake_root
+        make -f Bootstrap.mak linux
+        cd ..
+    else
+        echo "$(tput setaf 2)$(tput bold)> [premake] already compiled$(tput sgr 0)"
+    fi
+
+    cp $premake_root/bin/release/premake5 premake5
+
 fi
